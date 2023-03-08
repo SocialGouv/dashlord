54066dd335e8dbe88af95e905d346fa8c8083180
Clément <clement@numericite.eu>
2023-03-08 10:57:57 +0100

    feat: xwiki rest api integration

--- actions/jdma/index.js
+++ actions/jdma/index.js
@@ -1,34 +1,35 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const getXWikiJdmaData = (id, startDate, endDate) => {
-  console.log(
-    JSON.stringify({
-      id,
-      answersTotal: 1524,
-      satisfaction: {
-        negative: 500,
-        neutral: 24,
-        positive: 1000,
+const getXWikiJdmaData = (
+  id,
+  startDate,
+  endDate,
+  usernameXWiki,
+  passwordXWiki
+) => {
+  fetch(
+    `https://observatoire.numerique.gouv.fr/rest/observatoire/avistats/aggregatebyday/demarche/${id}?media=json&question=easy&question=satisfaction&question=comprehensible&date_start=${startDate}&date_end=${endDate}`,
+    {
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(`${usernameXWiki}:${passwordXWiki}`).toString("base64"),
       },
-      easy: {
-        negative: 200,
-        neutral: 300,
-        positive: 400,
-      },
-      comprehensible: null,
-      dateStart: startDate,
-      dateEnd: endDate,
-    })
-  );
+    }
+  ).then((response) => {
+    console.log(response);
+  });
 };
 
 module.exports = { getXWikiJdmaData };
 
 if (require.main === module) {
   getXWikiJdmaData(
-    process.argv[process.argv.length - 1],
+    process.argv[process.argv.length - 5],
+    process.argv[process.argv.length - 4],
+    process.argv[process.argv.length - 3],
     process.argv[process.argv.length - 2],
-    process.argv[process.argv.length - 3]
+    process.argv[process.argv.length - 1]
   );
 }
