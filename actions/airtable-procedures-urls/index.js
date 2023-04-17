880eead5daf0beebcddb18168a6e50ce25bae5e7
Clément <clement@numericite.eu>
2023-04-17 10:50:42 +0200

    feat: new column ID name

--- actions/airtable-procedures-urls/index.js
+++ actions/airtable-procedures-urls/index.js
@@ -1,25 +1,25 @@
 const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const urlRegex =
   /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 const field_names = {
-  id: "🕶 ID",
-  link: "Lien",
-  edition: "📡 Édition",
-  jdmaStartDate: "[Dashlord] - JDMA à partir de",
-  jdmaEndDate: "[Dashlord] - JDMA jusqu'à",
+  id: 'ID',
+  link: 'Lien',
+  edition: '📡 Édition',
+  jdmaStartDate: '[Dashlord] - JDMA à partir de',
+  jdmaEndDate: "[Dashlord] - JDMA jusqu'à"
 };
 
 const repeatRequest = async (url, headers, filters, offset, records = []) => {
   return fetch(
     `${url}?${new URLSearchParams(filters).toString()}${
-      offset ? `${filters ? "&" : ""}offset=${offset}` : ""
+      offset ? `${filters ? '&' : ''}offset=${offset}` : ''
     }`,
     {
-      headers,
+      headers
     }
-  ).then((response) => {
-    return response.json().then((json) => {
+  ).then(response => {
+    return response.json().then(json => {
       if (json.offset) {
         return repeatRequest(
           url,
@@ -47,10 +47,10 @@ const getAirtableUrls = async (
   let response = await repeatRequest(
     `https://api.airtable.com/v0/${base_id}/${editions_table_name}`,
     {
-      Authorization: `Bearer ${api_key}`,
+      Authorization: `Bearer ${api_key}`
     },
     {
-      filterByFormula: `{Éditions} = 'Édition actuelle'`,
+      filterByFormula: `{Éditions} = 'Édition actuelle'`
     }
   );
 
@@ -64,26 +64,26 @@ const getAirtableUrls = async (
   response = await repeatRequest(
     `https://api.airtable.com/v0/${base_id}/${procedures_table_name}`,
     {
-      Authorization: `Bearer ${api_key}`,
+      Authorization: `Bearer ${api_key}`
     },
     {
       // filterByFormula: `AND({${field_names.id}} = '1860' ,FIND('Édition actuelle', ARRAYJOIN({${field_names.edition}})))`,
-      filterByFormula: `FIND('Édition actuelle', ARRAYJOIN({${field_names.edition}}))`,
+      filterByFormula: `FIND('Édition actuelle', ARRAYJOIN({${field_names.edition}}))`
     }
   );
 
   console.log(
     JSON.stringify(
       response
-        .map((record) => ({
+        .map(record => ({
           id: record.fields[field_names.id],
           link: record.fields[field_names.link]
-            ? record.fields[field_names.link].replaceAll("\n", "")
-            : "",
+            ? record.fields[field_names.link].replaceAll('\n', '')
+            : '',
           startDate,
-          endDate,
+          endDate
         }))
-        .filter((record) => !!record.id && urlRegex.test(record.link)),
+        .filter(record => !!record.id && urlRegex.test(record.link)),
       null,
       2
     )
